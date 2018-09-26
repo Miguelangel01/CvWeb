71255dd00f728790174b886e7cd02c573e719e35
Miguel Angel Roig Santos <miguel.roig.s@gmail.com>
2018-09-26 14:23:09 +0200

    CvWeb
    Pequeños cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './body/body.component';
 import { Seccion1Component } from './seccion1/seccion1.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NameEditorComponent } from './name-editor/name-editor.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 
 @NgModule({
@@ -15,9 +13,7 @@ import { ProfileEditorComponent } from './profile-editor/profile-editor.componen
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent,
     Seccion1Component,
-    NameEditorComponent,
     ProfileEditorComponent
   ],
   imports: [
